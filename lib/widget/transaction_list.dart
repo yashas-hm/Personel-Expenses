0558d434dbca12343b5ab455f6cb8408ecf335eb
Yashas Majmudar <yashas-hm@users.noreply.github.com>
2021-01-03 21:22:29 +0530

    update

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -35,54 +35,80 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
+                  elevation: 5,
                   margin: EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 5,
+                    vertical: 8,
+                    horizontal: 5,
                   ),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        constraints: BoxConstraints(minWidth: 80),
-                        child: Text(
-                          '₹${_userTransactionList[index].amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 15,
-                        ),
-                        padding: EdgeInsets.all(5),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('₹${_userTransactionList[index].amount}'),
                         ),
                       ),
-                      Column(
-                        children: <Widget>[
-                          Text(
-                            _userTransactionList[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd()
-                                .format(_userTransactionList[index].timeStamp),
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      _userTransactionList[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd()
+                          .format(_userTransactionList[index].timeStamp),
+                    ),
                   ),
                 );
+                // return Card(
+                //   margin: EdgeInsets.symmetric(
+                //     horizontal: 20,
+                //     vertical: 5,
+                //   ),
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         constraints: BoxConstraints(minWidth: 80),
+                //         child: Text(
+                //           '₹${_userTransactionList[index].amount}',
+                //           style: TextStyle(
+                //               fontWeight: FontWeight.bold,
+                //               fontSize: 20,
+                //               color: Theme.of(context).primaryColor),
+                //         ),
+                //         margin: EdgeInsets.symmetric(
+                //           horizontal: 10,
+                //           vertical: 15,
+                //         ),
+                //         padding: EdgeInsets.all(5),
+                //         alignment: Alignment.center,
+                //         decoration: BoxDecoration(
+                //           border: Border.all(
+                //             color: Theme.of(context).primaryColor,
+                //             width: 2,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         children: <Widget>[
+                //           Text(
+                //             _userTransactionList[index].title,
+                //             style: Theme.of(context).textTheme.headline6,
+                //           ),
+                //           Text(
+                //             DateFormat.yMMMd()
+                //                 .format(_userTransactionList[index].timeStamp),
+                //             style: TextStyle(
+                //               fontSize: 12,
+                //               color: Colors.grey,
+                //             ),
+                //           ),
+                //         ],
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //       ),
+                //     ],
+                //   ),
+                // );
               },
               itemCount: _userTransactionList.length,
             ),
